24d1b4e51b15dc08e8b1392f6a208d322d946b99
Jlestat <jlestatt@gmail.com>
2024-01-25 21:49:01 +0300

    styles described

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>To-Do List</title>
-    <link rel="stylesheet" href="css/">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="main_section">
-        <div class="add_section">
+    <div class="main-section">
+        <div class="add-section">
             <form action="">
                 <input type="text" name="title" placeholder="Required field">
                 <button type="submit">Add</button>
             </form>
         </div>
-        <div class="show_todo_section">
-            <div class="todo_item">
+        <div class="show-todo-section">
+            <div class="todo-item">
                 <input type="checkbox">
                 <h2>This is something</h2>
                 <small>created: 25.01.2024</small>
