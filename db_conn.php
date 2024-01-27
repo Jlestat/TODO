77aa8698a049891eddf97cfba6b089f3a8f0bc18
Jlestat <jlestatt@gmail.com>
2024-01-27 19:42:48 +0300

    create add func

--- db_conn.php
+++ db_conn.php
@@ -8,7 +8,7 @@ $db_name = 'to_do_list';
 
 
 try{
-    $conn = new PDO("mysql:host=$sName;db_name=$db_name",
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name",
                         $uName, $pass);
     $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch (PDOException $e){
--- index.php
+++ index.php
@@ -15,27 +15,42 @@
 <body>
     <div class="main-section">
         <div class="add-section">
-            <form action="">
-                <input type="text" name="title" placeholder="Required field">
-                <button type="submit">Add</button>
+            <form action="app/add.php" method="POST" autocomplete="off">
+                <?php if  (isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
+                    <input type="text" name="title" style="border-color: #ff6666" placeholder="Required field">
+                    <button type="submit">Add</button>
+                <?php } else{?>
+                    <input type="text" name="title" placeholder="Required field">
+                    <button type="submit">Add</button>
+                <?php } ?>
             </form>
         </div>
         <?php
             $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
         ?>
         <div class="show-todo-section">
-            <?php if ($todos->rowCount() === 0) {?>
+            <?php if ($todos->rowCount() <=0 ) {?>
                 <div class="todo-item">
-                    <input type="checkbox">
-                    <h2>This is something</h2>
-                    <small>created: 25.01.2024</small>
+                    <div class="empty">
+                        <img src="img/f.png" width="100%"/>
+                        <img src="img/Ellipsis.gif" width="80px">
+                    </div>
                 </div>
             <?php }?>
-            <div class="todo-item">
-                <input type="checkbox">
-                <h2>This is something</h2>
-                <small>created: 25.01.2024</small>
-            </div>
+            <?php while ($todo = $todos->fetch(PDO::FETCH_ASSOC)) { ?>
+                <div class="todo-item">
+                    <span id="<?php echo $todo['id'];?>" class="remove-to-do">x</span>
+                    <?php if ($todo['checked']) { ?>
+                        <input type="checkbox" class="check-box" checked />
+                        <h2 class="checked"><?php echo $todo['title'] ?></h2>
+                    <?php } else { ?>
+                        <input type="checkbox" class="check-box"/>
+                        <h2><?php echo $todo['title'] ?></h2>
+                    <?php } ?>
+                    <br>
+                    <small>created:<?php echo $todo['date_time'] ?></small>
+                </div>
+            <?php } ?>
         </div>
     </div>
 </body>
